d1bdb82e4fc6fd58cc647fc08fb93112af1f3f8c
Antoine Tavernier <antoine.tavernier@epita.fr>
2015-10-16 22:43:30 +0200

    first commit

---
+++ grey_scale.c
@@ -0,0 +1,11 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <SDL.h>
+# include <SDL_image.h>
+
+int main()
+{
+
+return 0;
+
+}
---
+++ main.c
@@ -0,0 +1,22 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <SDL.h>
+# include <SDL_image.h>
+
+//# include image treatment
+# include "grey_sclale.h"
+# include "normalization.h"
+# include "integral_img.h"
+# include "equalization"
+
+//# include else
+//
+//
+
+
+int main()
+{
+
+return 0;
+
+}
