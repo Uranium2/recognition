66a3b6d2efa47456570e823a65669b009126e7cf
eymeric guyot <eymeric.guyot@epita.fr>
2015-11-01 03:27:47 +0100

    Redoing haar features, better results, better optimization

--- haar.c
+++ haar.c
@@ -2,108 +2,65 @@
 # include <stdio.h>
 # include <SDL.h>
 # include <SDL_image.h>
+# include "haar.h"
 
-int*  Haar_feature_1 (int *MHaar, int **M, SDL_Surface *img){
-
-	int scalex = 0, scaley = 0;
-
-	while ( 2 + scalex < img->w - 1 && 1 + scaley < img->h - 1){
-
-		for (int j = 0; j < img->h - (1 + scaley); j++){
-			for (int i = 0; i < img->w - (2 + scalex); i++ ){
-				*MHaar = M[i][j]- M[i][1 + j + scaley]- 2 * M[1 + i + scalex][j] + 2 * M[1 + i + scalex][1 + j + scaley] + M[2 + i + scalex][j] - M[2 + i + scalex][2 + j + scaley];
-				MHaar++;
-			}
-		}
-		scalex += 2;
-		scaley++;
-	}
-
-	return MHaar;
-
+void haar1(HaarF *haar, int **M){
+}
+void haar2(HaarF *haar, int **M){
+}
+void haar3(HaarF *haar, int **M){
+}
+void haar4(HaarF *haar, int **M){
+}
+void haar5(HaarF *haar, int **M){
 }
 
-int* Haar_feature_2 (int *MHaar, int **M, SDL_Surface *img){
-	int scalex = 0, scaley = 0;
-
-	while ( 1 + scalex < img->w - 1 && 2 + scaley < img->h - 1){
-
-		for (int j = 0; j < img->h - (2 + scaley)-1; j++){
-			for (int i = 0; i < img->w - (1 + scalex)-1; i++){ 
-				*MHaar = M[i][j] + 2 * M[1+ i + scalex][1 + j + scaley] - 2 *  M[i][1 + j + scaley] - M[1 + i + scalex][j] - M[1 + i + scalex][2 + j + scaley] + M[2 + i + scalex][1 + j + scaley];
-				MHaar++;
-			}
-		}
-		scaley += 2;
-		scalex++;
-	}
-
-	return MHaar;
-
-}
-
-int* Haar_feature_3 (int *MHaar, int **M, SDL_Surface *img){
-	int scalex = 0, scaley = 0;
-
-	while ( 3 + scalex < img->w -1 && 1 + scaley < img->h -1 ){
-		for (int j = 0; j < img->h - (1 + scaley); j++){
-			for (int i = 0; i < img->w - ( 3 + scalex); i++){
-				*MHaar = M[i][j] + 2 * M[1 + i + scalex][1 + j + scaley] - M[i][1 + j + scaley] - 2 * M[1 + i + scalex][j] + 2 * M[2 + i + scalex][j] + M[3 + i + scalex][1 + j + scaley] 
-					- 2 * M[2 + i + scalex][1 + j + scaley] - M[3 + i + scalex][j];
-				MHaar++;
-			}
-		}
-		scalex += 3;
-		scaley ++;
-	}
-	return MHaar;   
-}
-
-int* Haar_feature_4 (int *MHaar, int **M, SDL_Surface *img){
-	int scalex = 0, scaley = 0;
-
-	while ( 1 + scalex < img->w -1 && 3 + scaley < img->h -1 ){
-		for (int j = 0; j < img->h - (3 + scaley); j++){
-			for (int i = 0; i < img->w - ( 1 + scalex); i++){
-				*MHaar = -(M[i][j]) + M[1 + i + scalex][j] - 2 * M[1 + i + scalex][1 + j + scaley] + 2 * M[1 + i + scalex][2 + j + scaley] - M[1 + i + scalex][3 + j + scaley] 
-					+ M[i][3 + j + scaley] - 2 * M[i][2 + j + scaley] + 2 * M[i][1 + j + scaley];
-				MHaar++;
-			}
-		}
-		scaley += 3;
-		scalex ++;
-	}
-	return MHaar;
-
-}
-
-int* Haar_feature_5 (int *MHaar, int **M, SDL_Surface *img){
-	int scalex = 0, scaley = 0;
-
-	while ( 2 + scalex < img->w -1 && 2 + scaley < img->h -1 ){
-		for (int j = 0; j < img->h - (2 + scaley); j++){
-			for (int i = 0; i < img->w - ( 2 + scalex); i++){
-				*MHaar = -(M[i][j]) + 2 * M[1 + i + scalex][j] - M[2 + i + scalex][j] + 2 * M[2 + i + scalex][1 + j + scaley] - M[2 + i + scalex][2 + j + scaley] 
-									       + 2 * M[1 + i + scalex][2 + j + scaley] - M[i][2 + j + scaley] + 2 * M[i][1 + j + scaley] - 4 * M[1 + i + scalex][1 + j + scaley]; 
-				MHaar++;
-			}
-		}
-		scalex += 2;
-		scaley += 2;
-	}
-	return MHaar;
-
-}
-
-int* haar(int **M, SDL_Surface *img)
+void HaarFeatures(int **M)
 {
-	int *temp;
-	int* M_Haar = malloc(sizeof(int) * 17000);
-	temp = Haar_feature_1(M_Haar, M, img);
-	temp = Haar_feature_2(temp, M, img);
-	temp = Haar_feature_3(temp,M,img);
-	temp = Haar_feature_4(temp,M,img);
-	temp = Haar_feature_5(temp,M,img);
-	return M_Haar;
+	int featureSize[5][2] = {{2,1},{1,2},{3,1},{1,3},{2,2}};	
+
+	for(int featType = 0; featType < 5; featType++){
+
+		HaarF *haar = malloc(sizeof(HaarF));
+		int w = featureSize[featType][0];
+		int h = featureSize[featType][1];
+
+		haar->x = 0;
+		haar->y = 0;
+		haar->w = w;
+		haar->h = h;
+
+		while (haar->y + haar->h <= 24){
+			while (haar->x + haar->w <= 24){
+				while (haar->h + haar->y <= 24){
+					while (haar->w + haar->x <= 24){
+						if (featType == 0){
+							haar1(haar,M);
+						}
+						else if (featType == 1){
+							haar2(haar,M);
+						}
+						else if (featType == 2){
+							haar3(haar,M);
+						}
+						else if (featType == 3){
+							haar4(haar,M);
+						}
+						else if (featType == 4){
+							haar5(haar,M);
+						}
+						haar->w += w; 
+					}
+					haar->h += h;
+					haar->w = w;
+				}
+				haar->h = h;
+				haar->x += 1;
+			}
+			haar->x = 1;
+			haar->y += 1;
+		}
+		free(haar);
+	}        
 
 }
--- haar.h
+++ haar.h
@@ -3,10 +3,28 @@
 # include <SDL.h>
 # include <SDL_image.h>
 
-void Haar_feature_1 (int *MHaar, int **M, SDL_Surface *img);
-void Haar_feature_2 (int *MHaar, int **M, SDL_Surface *img);
-void Haar_feature_3 (int *MHaar, int **M, SDL_Surface *img);
-void Haar_feature_4 (int *MHaar, int **M, SDL_Surface *img);
-void Haar_feature_5 (int *MHaar, int **M, SDL_Surface *img);
+#ifndef HAAR
+#define HAAR
 
-int* haar(int **M, SDL_Surface *img);
+typedef struct HaarF HaarF;
+struct HaarF
+
+{
+
+	int x;
+	int y;
+	int w;
+	int h;
+	int val;
+
+};
+
+#endif
+
+void haar1(HaarF *haar, int **M);
+void haar2(HaarF *haar, int **M);
+void haar3(HaarF *haar, int **M);
+void haar4(HaarF *haar, int **M);
+void haar5(HaarF *haar, int **M);
+
+void HaarFeatures(int **M);
