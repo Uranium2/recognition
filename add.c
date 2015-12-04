bcbaca699850151c9602c94e0e2a2b95d98ac544
Alixe Aymeric <aymeric.alixe@epita.fr>
2015-12-04 01:19:09 +0100

    avanceinterface

--- add.c
+++ add.c
@@ -4,22 +4,25 @@
 #include <dirent.h>
 #include <string.h>
 
-void add(char* pathImg , char* pathBdd){
-  
-DIR* testBdd = NULL;
+void add(char* pathImg , char* pathBdd)
+{  
+  DIR* testBdd = NULL;
   testBdd = opendir(pathBdd);
-  if(testBdd == NULL){
+  if (testBdd == NULL){
     printf("Wrong path for Data base \n");
     closedir(testBdd);
     return;
   }
   char* ext = strchr(pathImg,'.');
-  if( strcmp(ext,"pgm")||strcmp(ext,"bmp")||strcmp(ext,"jpeg")||strcmp(ext,"jpg")||strcmp(ext,"png")){
-    system("mv pathImg pathBdd");
+  if (strcmp(ext,"pgm") || strcmp(ext,"bmp") || strcmp(ext,"jpeg") || strcmp(ext,"jpg") || strcmp(ext,"png")){
+    char *pathmove = "mv %s %s",pathImg,pathBdd;
+    system(pathmove);
     closedir(testBdd);
   }
   printf("Image's format isn't supported \n");
   closedir(testBdd);
   return;
 }
+
+int main(){return 0;}
       
